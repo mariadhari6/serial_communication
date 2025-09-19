4ee486ad235cc77e3790ac24e4ce223b413f5057
Mar'i Adhari <mariadhari6@gmail.com>
2025-09-19 14:41:36 +0700

    add sequence, checksum & retry mechanism

--- client/Program.cs
+++ client/Program.cs
@@ -1,38 +1,43 @@
 ﻿using System;
 using System.IO.Ports;
-using System.Runtime.CompilerServices;
 using System.Text;
 using DotNetEnv;
-using System.Linq;
+using System.Reflection.Metadata;
 public class Program
 {
 
-  private static string TEXT = """
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget laoreet eros. In ante turpis, venenatis in condimentum id, scelerisque a nisi. Praesent ullamcorper scelerisque lectus, nec mollis ipsum finibus sed. Pellentesque congue felis nibh, sit amet hendrerit mi consequat at. Proin tempor quis turpis non semper. Ut vel diam aliquam, rhoncus enim eget, accumsan est. Nunc tempus tincidunt sapien ac tincidunt. Morbi ac iaculis odio. Aenean tristique sagittis risus, euismod vehicula nulla sagittis facilisis. Quisque orci massa, sollicitudin sit amet eros a, finibus tempus mi. Aenean maximus dapibus pharetra. Nam tempor auctor massa, nec iaculis diam facilisis ac. Sed posuere, nunc eget gravida suscipit, velit mi blandit quam, a elementum odio velit ac justo. Aenean orci felis, aliquet non dolor eu, vestibulum posuere nisl. Etiam at aliquet enim, ut pulvinar est. Maecenas in malesuada elit 
-        Halo Dunia
-        Hello Morasaurus
-        Hello Dinosaurus
-        こんにちは世界
-        你好，世界
+  private static readonly string TEXT = """
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget laoreet eros. 
+  Halo Dunia
+  Hello Morasaurus
+  Hello Dinosaurus
+  こんにちは世界
+  你好，世界
   """;
-  private static byte ENQ = 5;
-  private static byte ACK = 6;
-  private static byte ETB = 23;
-  private static byte EOT = 4;
+  private static readonly byte ENQ = 5;
+  private static readonly byte ACK = 6;
+  private static readonly byte ETB = 23;
+  private static readonly byte EOT = 4;
   private static int line = 0;
-  private static byte STX = 2;
-  private static byte ETX = 3;
-  private static byte CR = 13;
+  private static readonly byte STX = 2;
+  private static readonly byte ETX = 3;
+  private static readonly byte CR = 13;
+  private static readonly byte LF = 10;
+  private static readonly byte NAK = 21;
   private static int partition = 0;
+  private static int lastPartition = partition;
 
-  private static int maxCharactersPerLine = 50;
+  private static readonly int maxCharactersPerLine = 50;
+  private static int sequence = 0;
+
+  private static readonly int MAX_SEQUENCE = 7;
 
   private static bool running = true;
   public static void Main(string[] args)
   {
 
     // Load env variable
-    DotNetEnv.Env.Load();
+    Env.Load();
     string portName = Environment.GetEnvironmentVariable("SERIAL_PORT") ?? "/dev/pts/1";
     int baudRate = int.Parse(Environment.GetEnvironmentVariable("BAUD_RATE") ?? "9600");
     int dataBits = int.Parse(Environment.GetEnvironmentVariable("DATA_BITS") ?? "8");
@@ -47,7 +52,7 @@ public class Program
     serialPort.Handshake = Handshake.None;
 
 
-    serialPort.DataReceived += new SerialDataReceivedEventHandler(ACKReceiveHandler);
+    serialPort.DataReceived += new SerialDataReceivedEventHandler(ReceiveHandler);
 
     try
     {
@@ -59,7 +64,7 @@ public class Program
 
       while (running)
       {
-        System.Threading.Thread.Sleep(100); // Wait for the program to finish sending all text
+        Thread.Sleep(100); // Wait for the program to finish sending all text
       }
       serialPort.Close();
     }
@@ -68,7 +73,7 @@ public class Program
       Console.WriteLine("Error: " + e.Message);
     }
   }
-  private static void ACKReceiveHandler(object sender, SerialDataReceivedEventArgs e)
+  private static void ReceiveHandler(object sender, SerialDataReceivedEventArgs e)
   {
     SerialPort sp = (SerialPort)sender;
     int data = sp.ReadByte();
@@ -76,6 +81,24 @@ public class Program
     {
       Console.WriteLine("Received ACK from Server.");
       SendText(sp);
+
+    }
+    else if (data == NAK)
+    {
+      Console.WriteLine("Received NAK from Server.");
+      // Resend the last text
+      // partition = Math.Max(0, partition - 1);
+      sequence = sequence == 0 ? MAX_SEQUENCE : sequence - 1;
+      if (partition == 0 && line > 0)
+      {
+        line = Math.Max(0, line - 1);
+      }
+      partition = lastPartition;
+      SendText(sp);
+    }
+    else
+    {
+      Console.WriteLine("Received unknown data from Server: " + data);
     }
   }
   private static string? GetText()
@@ -83,6 +106,7 @@ public class Program
     string[] lines = TEXT.Split('\n');
     if (line < lines.Length)
     {
+      lastPartition = partition;
       string text;
       if (lines[line].Length <= maxCharactersPerLine)
       {
@@ -107,6 +131,18 @@ public class Program
     }
     return null;
   }
+  private static string GenerateCheckSum(byte[] packet, byte endtx)
+  {
+    int indexSTX = Array.IndexOf(packet, STX);
+    int indexENDTX = Array.IndexOf(packet, endtx);
+    if (indexSTX == -1 || indexENDTX == -1 || indexENDTX <= indexSTX)
+    {
+      throw new ArgumentException("Invalid packet format");
+    }
+    List<byte> contentBytes = [.. packet[(indexSTX + 1)..indexENDTX]];
+    string checkSum = (contentBytes.Count % 256).ToString("X2");
+    return checkSum;
+  }
   private static void SendText(SerialPort sp)
   {
     string? text = GetText();
@@ -123,18 +159,54 @@ public class Program
       return;
     }
 
-    List<byte> packet = new List<byte>();
+    List<byte> packet;
+    byte[] content = Encoding.UTF32.GetBytes(text);
+    string checkSum;
+    bool simulationMode = Environment.GetEnvironmentVariable("SIMULATION_MODE") == "true";
+    int currentSequence = sequence;
+    if (simulationMode && new Random().Next(0, 2) == 0)
+    {
+      // simulate error by changing sequence number to 9
+      // random booleancurrentSequence = 9;
+      currentSequence = 9;
+      Console.WriteLine("Simulation mode: sending wrong sequence number");
+    }
     if (partition > 0)
     {
       // [STX][TEXT][ETB]
-      packet = [STX, .. Encoding.UTF32.GetBytes(text), ETB];
+      packet = [STX, (byte)currentSequence, .. content, ETB];
+      checkSum = GenerateCheckSum([.. packet], ETB);
     }
     else
     {
       // [STX][TEXT][CR][ETX]
-      packet = [STX, .. Encoding.UTF32.GetBytes(text), CR, ETX];
+      packet = [STX, (byte)currentSequence, .. content, CR, ETX];
+      checkSum = GenerateCheckSum([.. packet], ETX);
     }
+    if (simulationMode && new Random().Next(0, 2) == 0)
+    {
+      // simulate error by changing checksum to "FF"
+      checkSum = "FF";
+      Console.WriteLine("Simulation mode: sending wrong checksum");
+    }
+
+    byte[] checkSumBytes = Encoding.UTF8.GetBytes(checkSum);
+    packet.AddRange([.. checkSumBytes, CR, LF]);
+
     sp.Write(packet.ToArray(), 0, packet.Count);
     Console.WriteLine("Sent text to Server: " + text.Trim());
+    if (sequence < MAX_SEQUENCE)
+    {
+      sequence++;
+    }
+    else
+    {
+      sequence = 0;
+    }
+  }
+
+  private static object Random()
+  {
+    throw new NotImplementedException();
   }
 }
